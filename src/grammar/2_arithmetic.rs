4dd8fa0e3970cbea141bbfb8099f8841556179f4
sinlov <sinlovgmppt@gmail.com>
2019-08-08 18:53:18 +0800

    update test case

--- src/grammar/2_arithmetic.rs
+++ src/grammar/2_arithmetic.rs
@@ -1,19 +1,19 @@
 #[test]
 fn addition() {
-    assert_eq!(1 + 1, 2)
+    assert_eq!(1 + 1, 2);
 }
 
 #[test]
 fn subtraction() {
-    assert_eq!(1 - 1, 0)
+    assert_eq!(1 - 1, 0);
 }
 
 #[test]
 fn multiplication() {
-    assert_eq!(2 * 3, 6)
+    assert_eq!(2 * 3, 6);
 }
 
 #[test]
 fn division() {
-    assert_eq!(6 / 2, 3)
+    assert_eq!(6 / 2, 3);
 }
---
+++ src/grammar/function_test.rs
@@ -0,0 +1,18 @@
+// more info see https://rustlang-cn.org/office/rust/book/advanced-features/ch19-01-unsafe-rust.html
+#[test]
+fn funtion_access_to_external_variables() {
+  let mut num = 5;
+  let r1 = &num as *const i32;
+  let r2 = &mut num as *mut i32;
+  // let mut one = 1;
+  for_external_variables(&mut *r2);
+  // println!("ret {}", one);
+  assert_eq!(*r1, 5);
+  assert_eq!(*r2, 10);
+}
+
+fn for_external_variables(n: &mut i32) {
+  unsafe {
+    n = &mut 10;
+  }
+}
